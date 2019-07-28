5055e8729a62b2bfff6d89fb387ff4eddfc5dbf9
Purnima Suryaprakash <purnima1999.surya@gmail.com>
2019-07-28 20:54:09 +0530

    Friendship meter test cases added

--- src/main/java/org/gfg/base/rest/BaseController.java
+++ src/main/java/org/gfg/base/rest/BaseController.java
@@ -16,7 +16,7 @@ public class BaseController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public <T> T listUsers() {
         Map<String, Object> map = new LinkedHashMap<String, Object>();
-        map.put("india1", "delhi");
+        map.put("india", "delhi");
         map.put("korea", "seoul");
         map.put("afghanistan", "kabul");
         map.put("canada", "ottawa");
@@ -37,6 +37,19 @@ public class BaseController {
         Map<String, Object> map = new LinkedHashMap<String, Object>();
         map.put("name", name);
         map.put("reverse_name", reverse);
+        
+        return (T) map;
+    }
+    
+    // http://localhost:8080/friendship-meter
+    @RequestMapping(value = "/friendship-meter", method = RequestMethod.GET)
+    public <T> T getFriendshipMeter(@RequestParam(value = "name1", defaultValue = "Gautam Karthik") String name1, @RequestParam(value = "name2", defaultValue = "Deepika Padukone") String name2) {
+    	int fMeter = (int)(Math.random() * 50 + 1);
+    	
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("name1", name1);
+        map.put("name2", name2);
+        map.put("friendship_meter", fMeter);
         
         return (T) map;
     }
